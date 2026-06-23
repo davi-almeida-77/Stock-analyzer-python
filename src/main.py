10363dc470c24ef7da26ef8a3443759259708e57
davi-almeida-77 <DaviAlmeidaDEV@hotmail.com>
2026-06-23 16:20:58 -0300

    [TESTS] first test of the dataframe with real Tickers

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from extractors.extractor_yfinance import StockExtractor
+from transformers.stock_transformer import StockTransformer
+
+extractor = StockExtractor(["NVDA", "PETR4.SA", "VALE3.SA", "AMD", "TSM", "ASML", "WEGE3.SA", "MGLU3.SA", "ITUB4.SA"], "1y")
+
+extractor.download()
+
+transformer = StockTransformer(extractor.data)
+
+df = transformer.transform()
+
+print(df)
