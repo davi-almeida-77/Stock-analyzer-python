abece3597b87215509f7259aa101540d4a0b619f
davi-almeida-77 <DaviAlmeidaDEV@hotmail.com>
2026-06-25 17:02:11 -0300

    [FEAT] Transformer W MA20 and MA50 indicators, daily, return and volatility

--- src/transformers/stock_transformer.py
+++ src/transformers/stock_transformer.py
@@ -7,4 +7,13 @@ class StockTransformer():
     def transform(self):
         df = pd.concat(objs=self.data.values(), keys=self.data.keys())
 
+        df["MM20"] = df.groupby(level=0)["Close"].transform(lambda x: x.rolling(20).mean())
+
+        df["MM50"] = df.groupby(level=0)["Close"].transform(lambda x: x.rolling(50).mean())
+
+        df["Diary_return"] = df.groupby(level=0)["Close"].transform(lambda x: x.pct_change())
+
+        df["Volatility"] = df.groupby(level=0)["Diary_return"].transform(lambda x: x.rolling(20).std())
+
+
         return df
