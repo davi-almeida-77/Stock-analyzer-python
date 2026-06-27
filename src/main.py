c9f8d2f1d3f674252fc3fc565b1caebfa250b8c8
davi-almeida-77 <DaviAlmeidaDEV@hotmail.com>
2026-06-26 23:35:32 -0300

    [ACTIVATE] excel export function in main file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from extractors.extractor_yfinance import StockExtractor
 from transformers.stock_transformer import StockTransformer
+from exporters.excel_exporter import ExcelExporter
 
 extractor = StockExtractor(["NVDA", "PETR4.SA", "VALE3.SA", "AMD", "TSM", "ASML", "WEGE3.SA", "MGLU3.SA", "ITUB4.SA", "^BVSP", "^SOX"], "1y")
 
@@ -8,5 +9,9 @@ extractor.download()
 transformer = StockTransformer(extractor.data)
 
 df = transformer.transform()
+
+excel_export = ExcelExporter(df)
+
+excel_export.CreateSheetStructure()
 
 print(df)
