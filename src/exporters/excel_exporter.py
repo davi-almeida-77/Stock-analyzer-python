78de67cd01d18de5bc6270a6c4cb78c07157b28d
davi-almeida-77 <DaviAlmeidaDEV@hotmail.com>
2026-06-28 01:31:55 -0300

    [FEAT] Resume tab with Solid metrics by Ticker

--- src/exporters/excel_exporter.py
+++ src/exporters/excel_exporter.py
@@ -21,17 +21,37 @@ class ExcelExporter():
         df_month = df_month.pct_change()
         df_month.index = df_month.index.set_levels(df_month.index.levels[1].date, level=1)
 
-        
-        
+        df_resume = df.groupby("Ticker").agg({
+            "Close": ["first", "last", "min", "max"],
+            "Volatility": "median"
+})
+        df_resume["Rise"] = (
+            df_resume[("Close", "max")] / df_resume[("Close", "min")] - 1
+        ) * 100
+
+        df_resume["Lower"] = (
+            df_resume[("Close", "min")] / df_resume[("Close", "max")] - 1
+        ) * 100
+
+        df_resume["Total_Return"] = (
+            df_resume[("Close", "last")] / df_resume[("Close", "first")] - 1
+        ) * 100
+
+        df_resume.columns = df_resume.columns.set_levels(
+            df_resume.columns.levels[1].str.capitalize(), level=1
+        )
 
         with pd.ExcelWriter(output) as writer:
             self.dataframe.to_excel(writer, sheet_name="History")
             df_month.to_excel(writer, sheet_name="Month_Return")
-            self.dataframe.to_excel(writer, sheet_name="Resume")
+            df_resume.to_excel(writer, sheet_name="Resume")
 
 
             for sheet_name in ["History", "Month_Return", "Resume"]:
                 worksheet = writer.sheets[sheet_name]
                 
                 for column in worksheet.columns:
-                    worksheet.column_dimensions[column[0].column_letter].width = 20
+                    try:
+                        worksheet.column_dimensions[column[0].column_letter].width = 20
+                    except AttributeError:
+                        pass
